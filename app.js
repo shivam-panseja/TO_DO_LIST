1267151eef5cf9fa2c9ae90241f68f0bdf1449bd
Shivam Panseja <shivam.panseja@thewitslab.com>
2024-09-10 18:15:26 +0530

    console.log("iam done")

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+const todos = [];
+
+// app.use('/Static', express.Static('Static'))
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded({ extended: true }));
+console.log("hi am here");
+
+// app.get("/home", (req, res) => {
+//   res.send("hooray working");
+// });
+
+// can place authentication here
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    name: "Shivam",
+    todos: todos,
+  });
+});
+
+app.post("/", (req, res) => {
+  // push new todo into array
+  todos.push(req.body);
+  // redirect back to main page (refresh page)
+  res.redirect("/");
+});
+// run your application, so it listens on port 4444
+app.listen(4444, () => {
+  console.log("Server is Listening on port 4444");
+});
+
+app.listen(3000, () => {
+  console.log("The Port is running on 3000");
+});
